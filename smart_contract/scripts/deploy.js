70a5f6c34a2b9e90b48894704b5efef5044696a5
Gyeonghun Park <zrhdnl@gmail.com>
2022-02-25 16:17:42 +0900

    Done Smart Contract

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -8,29 +8,15 @@ const main = async () => {
       "QmSiW2wcvf3zYkKdV5EmYXm9HxRhGPw8tqKyijF83wHtVi",
     ],
     [100, 200, 300],
-    [100, 50, 25]
+    [100, 50, 25],
+    "Elon Musk", // Boss name
+    "Qmens3TSDu2rpf7KSdoNGZdnLkN9CnS2mX7eyt6KUF4uNi", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
+
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
-
-  let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-  console.log("Minted NFT #3");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #4");
-
-  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
